b71ffa52fd96d0547615368e21cd3f2d6b3f97be
kmg733 <mang906@naver.com>
2022-01-11 12:46:43 +0900

    About Method

--- django.py
+++ django.py
@@ -1,11 +1,17 @@
 class Car():
-    wheels = 4
-    doors = 4
-    windows = 4
-    seats = 4
 
-porche = Car()
-porche.color = "Red"
+    def __init__(self, *args, **kwargs):
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4
+        self.color = kwargs.get("color", "black")
+        self.price = kwargs.get("price", "$20")
+    def __str__(self):
+        return f"Car wit {self.wheels} wheels"
 
-ferrari = Car()
-ferrari.color = "Yellow"
+porche = Car(color="green", price="$40")
+print(porche.color, porche.price)
+
+mini = Car()
+print(mini.color, mini.price)
