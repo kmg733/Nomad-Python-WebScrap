459eca3323a181a9413a6ccd19435619b249d32e
kmg733 <mang906@naver.com>
2022-01-11 12:54:54 +0900

    Extending Classes

--- django.py
+++ django.py
@@ -1,5 +1,4 @@
 class Car():
-
     def __init__(self, *args, **kwargs):
         self.wheels = 4
         self.doors = 4
@@ -7,11 +6,23 @@ class Car():
         self.seats = 4
         self.color = kwargs.get("color", "black")
         self.price = kwargs.get("price", "$20")
+
     def __str__(self):
-        return f"Car wit {self.wheels} wheels"
+        return f"Car with {self.wheels} wheels"
 
-porche = Car(color="green", price="$40")
-print(porche.color, porche.price)
+# 상위 클래스 상속
+class Conventable(Car):
+    def __init__(self, *args, **kwargs):
+        # 상위 클래스의 메소드 상속 없으면 오버라이딩
+        super().__init__(**kwargs)
+        self.time = kwargs.get("time", 10)
 
-mini = Car()
-print(mini.color, mini.price)
+    def take_off(self):
+        return "taking off"
+
+    def __str__(self):
+        return f"Car with no roof"
+
+
+porche = Conventable(color="green", price="$40")
+print(porche.color)
