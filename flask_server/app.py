ef90abe30601f80cf53ebca9df2699d0203874cd
kmg733 <mang906@naver.com>
2022-01-20 17:53:44 +0900

    Rendering Jobs With Flask

--- flask_server/app.py
+++ flask_server/app.py
@@ -3,6 +3,9 @@ from scrapper import getJobs
 
 app = Flask("webScrapper", template_folder='./flask_server/templates')
 
+# fakeDB
+db = {}
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -13,11 +16,20 @@ def report():
     # 사용자 입력 formating
     if word:
         word = word.lower()
-        jobs = getJobs(word)
-        print(jobs)
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = getJobs(word)
+            db[word] = jobs
     # word가 None일경우 처리
     else:
         return redirect("/")
-    return render_template("report.html", searchingBy=word)
+    return render_template(
+        "report.html",
+        searchingBy=word,
+        resultsNumber=len(jobs),
+        jobs=jobs
+    )
 
 app.run(debug=True)
